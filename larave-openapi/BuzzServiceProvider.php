a20de3b94af9a0e48d91697c12c21c5fd7c872d0
buzz <372976541@qq.com>
2017-05-18 17:28:53 +0800

    update buzz

---
+++ larave-openapi/BuzzServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+namespace Hello\LaravelOpenApi;
+
+use Hello\OpenApi\Buzz;
+use Illuminate\Foundation\Application as LaravelApplication;
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+class BuzzServiceProvider extends LaravelServiceProvider
+{
+    /**
+     * 延迟加载.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Boot the provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //在这里还可以做一些业务初始化
+        $this->app->singleton(Buzz::class, function ($laravelApp) {
+            $app = new Buzz;
+            return $app;
+        });
+        $this->app->alias(Buzz::class, 'Buzz');
+    }
+
+    /**
+     * 提供的服务
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['Buzz', Buzz::class];
+    }
+}
---
+++ openapi/Buzz.php
@@ -0,0 +1,13 @@
+<?php
+namespace Hello\OpenApi;
+
+class Buzz{
+
+	public function __construct(){
+
+	}
+
+	public function buzz(){
+		return 'hello buzz';
+	}
+}
